479bc822ff1428339bd588663e387d153e32f2d1
danielnoo <dannynoo@gmail.com>
2021-09-14 21:12:05 -0400

    added more assets, trying to get shark to move on cursor so it can move over safi

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,59 @@
 console.log('hello')
+
+
+
+
+
+
+
+
+// a function that finds the client's mouse X coordinate
+function getMouseX (event) {
+  console.log(event.offsetX);
+  const shark = document.querySelector('.shark');
+  shark.style.left = event.offsetX;
+};
+
+
+document.addEventListener('mousemove', getMouseX);
+
+
+
+
+
+
+
+
+
+const waveArray = document.querySelectorAll('.wave');
+const delayArray = [1000, 2000, 2500, 6000]
+
+
+
+
+
+
+
+
+
+
+
+setInterval(() => {
+  waveArray.forEach((wave, index) => {
+    setTimeout(() => {
+      wave.classList.toggle('wave-transition');
+    },delayArray[index])
+  })
+},4000)
+
+
+
+
+function startWave () {
+  let wave = document.querySelector('.wave3');
+  wave.classList.toggle('wave-transition');
+  setTimeout(() => {
+    
+    startWave()
+  }, 3000)
+};
