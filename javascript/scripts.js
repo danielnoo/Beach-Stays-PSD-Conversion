b0c7cce3fa813af9492d081775b8bc8885a374b2
danielnoo <dannynoo@gmail.com>
2021-09-14 22:54:13 -0400

    have basic functionality down, need to clean up code

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,59 +1,44 @@
-console.log('hello')
+
+// audio declared outside of the calling function to avoid multiple 
+// simultaneous plays
+const audio = new Audio("../assets/theme.mp3")
+document.addEventListener('click', () => {
+  const headingOne = document.querySelector('#headingOne');
+  const headingTwo = document.querySelector('#headingTwo');
+  setTimeout(() => audio.play(), 2000)
+  setTimeout(() => headingOne.style.visibility = 'visible', 4500)
+  setTimeout(() => headingTwo.style.visibility = 'visible', 4800)
+})
 
 
-
-
-
-
-
-
-// a function that finds the client's mouse X coordinate
-function getMouseX (event) {
-  console.log(event.offsetX);
+// a function that finds the client's mouse X coordinate and moves the shark
+const getMouseX = (event) => {
   const shark = document.querySelector('.shark');
-  shark.style.left = event.offsetX;
+  shark.style.left = event.offsetX + 'px';
+  shark.style.top = '200px';
 };
-
-
 document.addEventListener('mousemove', getMouseX);
 
 
 
 
+/// a function that moves the waves, slowly dragging safi into the tide
+
+const moveyWaves = () => {
+  const waveArray = document.querySelectorAll('.wave');
+  const delayArray = [1000, 2000, 2500, 6000]
 
 
-
-
-
-const waveArray = document.querySelectorAll('.wave');
-const delayArray = [1000, 2000, 2500, 6000]
-
-
-
-
-
-
-
-
-
-
-
-setInterval(() => {
-  waveArray.forEach((wave, index) => {
-    setTimeout(() => {
-      wave.classList.toggle('wave-transition');
-    },delayArray[index])
-  })
-},4000)
-
-
-
-
-function startWave () {
-  let wave = document.querySelector('.wave3');
-  wave.classList.toggle('wave-transition');
-  setTimeout(() => {
-    
-    startWave()
-  }, 3000)
+  setInterval(() => {
+    waveArray.forEach((wave, index) => {
+      setTimeout(() => {
+        wave.classList.toggle('wave-transition');
+      },delayArray[index]);
+    });
+  },4000);
 };
+
+moveyWaves();
+
+
+
